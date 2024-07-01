ca8007c88e2d6ed019510c05270be23378a91454
Rabin Otieno <48247410+Rabinnnn@users.noreply.github.com>
2024-07-01 15:04:53 +0300

    Create Searchreplace.go
    A program that takes 3 arguments, the first argument is a string in which a letter (the 2nd argument) will be replaced by another one (the 3rd argument).

---
+++ Searchreplace.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	//"strings"
+	//"strconv"
+)
+
+func main(){
+	if len(os.Args) != 4{
+		return
+	}
+	fmt.Println(srep(os.Args[1],os.Args[2],os.Args[3]))
+}
+
+func srep(s1 string, s2 string, s3 string)string{
+	output := ""
+	r1 := []rune(s1)
+	r2 := []rune(s2)
+	r3 := []rune(s3)
+	fmt.Println(string(r2[0]))
+	fmt.Println(string(r3[0]))
+	for _, char := range r1{
+		if char == r2[0]{
+			char = r3[0]
+		}
+		output += string(char)
+	}
+	return output
+}
